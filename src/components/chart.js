d5f1f26ca48348c266d20a2b481440c7a99c5d71
Gary Clark <Gary Clark>
2017-08-29 20:28:05 -0500

    using react-jsx-highcharts

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,56 +1,15 @@
 import React, { Component } from 'react';
-// Note that Highcharts has to be required separately
-import ReactHighcharts from 'react-highcharts';
 import axios from 'axios';
 import _ from 'lodash';
 
 class Bars extends Component {
 
-    // componentDidMount = () => {
-    //     this.setSeries();
-    // }
-
-    // setSeries = () => {
-    //     axios.get(`/api/data`)
-    //         .then(response => {
-    //             this.props.config.xAxis.categories = response.data.categories
-    //             this.props.config.series = response.data.series
-    //             this.setState({
-    //                 config: this.props.config
-    //             })
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }
 
     render() {
 
-        const getData = () => {
-            axios.get(`/api/data`)
-                .then(response => {
-                    this.props.config.xAxis.categories = response.data.categories
-                    this.props.config.series = response.data.series
-                    // this.props.config
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        }
-
-        getData()
-        // this.props.config = _.isUndefined(this.props.config.series) ? getData() : this.props.config
-
-        this.props.config.xAxis.categories = _.isUndefined(this.props.data) ? this.props.config.xAxis.categories : this.props.data.categories;
-        this.props.config.series = _.isUndefined(this.props.data) ? this.props.config.series : this.props.data.series
-
-        // console.log(ReactHighcharts.Highcharts.getOptions())
 
         return (
-            <ReactHighcharts
-                config={this.props.config}
-                ref="chart"
-            />
+            
         )
     }
 }
